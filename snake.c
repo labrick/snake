f99665ae3f6a8d1aa0746dbbd93bf40850f1021c
Yang Zhang <tianna1121@gmail.com>
2016-04-10 21:33:53 +0800

    大概注释了下，后面再整理

--- snake.c
+++ snake.c
@@ -9,36 +9,41 @@
 #define DESIRED_HEIGHT 25
 
 WINDOW * g_mainwin;
-int g_oldcur, g_score = 0, g_width, g_height;
-typedef struct {
+int g_oldcur;
+int g_score = 0;    // 分值
+int g_width;        // 边框的宽度
+int g_height;       // 边框的高度
+
+typedef struct {	// 蛇体关节位置
     int x;
     int y;
 } pos;
-pos fruit;
+pos fruit;		    // 单个食物的位置
 
 // 2D array of all spaces on the board.
-bool *spaces;
+bool *spaces;		// 这个指针呢？
 
 // --------------------------------------------------------------------------
 // Queue stuff
 
 // Queue implemented as a doubly linked list
-struct s_node
+struct s_node		// 记录蛇体
 {
-    pos *position; // **TODO: make this a void pointer for generality.
+// **TODO: make this a void pointer for generality.
+    pos *position;          // 蛇体每个关节的位置
     struct s_node *prev;
     struct s_node *next;
-} *front=NULL, *back=NULL;
+} *front=NULL, *back=NULL;  // 蛇体的头和尾
 typedef struct s_node node;
 
 // Returns the position at the front w/o dequeing
-pos* peek( )
+pos* peek(void)
 {
     return front == NULL ? NULL : front->position;
 }
 
 // Returns the position at the front and dequeues
-pos* dequeue( )
+pos* dequeue(void)
 {
     node *oldfront = front;
     front = front->next;
@@ -46,7 +51,8 @@ pos* dequeue( )
 }
 
 // Queues a position at the back
-void enqueue( pos position )
+// 在back后加入一个新的位置元素
+void enqueue(pos position)
 {
    pos *newpos   = (pos*)  malloc( sizeof( position ) ); 
    node *newnode = (node*) malloc( sizeof( node ) );
@@ -77,86 +83,93 @@ void snake_write_text( int y, int x, char* str )
 }
 
 // Draws the borders
-void snake_draw_board( )
+// 画边框
+void snake_draw_board(void)
 {
     int i;
-    for( i=0; i<g_height; i++ )
+    for( i=0; i<g_height; i++ )     // 在0列和g_width列划线
     {
         snake_write_text( i, 0,         "X" );
         snake_write_text( i, g_width-1, "X" );
     }
-    for( i=1; i<g_width-1; i++ )
+    for( i=1; i<g_width-1; i++ )    // 在0行和g_height列划线
     {
         snake_write_text( 0, i,          "X" );
         snake_write_text( g_height-1, i, "X" );
     }
+    // 隔一行后写入分数：
     snake_write_text( g_height+1, 2, "Score:" );
 }
 
 // Resets the terminal window and clears up the mem
-void snake_game_over( )
+void snake_game_over(void)
 {
-    free( spaces );
-    while( front )
+    free(spaces);       // spaces这个指针干嘛的？
+    while(front)        // 释放蛇体所占空间
     {
         node *n = front;
         front = front->next;
-        free( n );
+        free(n);
     }
-    endwin();
+    endwin();           // 关闭窗口
     exit(0);
 }
 
 // Is the current position in bounds?
-bool snake_in_bounds( pos position )
+// 检测是否在边界内
+bool snake_in_bounds(pos position)
 {
-    return position.y < g_height - 1 && position.y > 0 && position.x < g_width - 1 && position.x > 0;
+    return ((position.y < g_height-1) && (position.y > 0) && (position.x < g_width-1) && (position.x > 0));
 }
 
 // 2D matrix of possible positions implemented with a 1D array. This maps
 // the x,y coordinates to an index in the array.
-int snake_cooridinate_to_index( pos position )
+// 将x,y两维值映射到一维上：width * y + x   为什么可以这么做？
+int snake_cooridinate_to_index(pos position)
 {
     return g_width * position.y + position.x;
 }
 
 // Similarly this functions maps an index back to a position
+// 和上面一个函数所做的事情相反
 pos snake_index_to_coordinate( int index )
 {
     int x = index % g_width;
     int y = index / g_width;
-    return (pos) { x, y };
+    return (pos){ x, y };
 }
 
 // Draw the fruit somewhere randomly on the board
+// 产生食物并设置其位置
 void snake_draw_fruit( )
 {
-    attrset( COLOR_PAIR( 3 ) );
+    attrset( COLOR_PAIR( 3 ) );     // 设置字符属性(颜色)
     int idx;
     do
     {
-        idx = rand( ) % ( g_width * g_height );
+        idx = rand() % (g_width * g_height);
         fruit = snake_index_to_coordinate( idx );
     }
+    // 如果食物位置不在边界内或者这个位置被蛇体占用，则死机
     while( spaces[idx] || !snake_in_bounds( fruit ) );    
     snake_write_text( fruit.y, fruit.x, "F" );
 }
 
 // Handles moving the snake for each iteration
-bool snake_move_player( pos head )
+bool snake_move_player(pos head)
 {
-    attrset( COLOR_PAIR( 1 ) ) ;
+    attrset(COLOR_PAIR(1));    // 设置颜色属性
     
     // Check if we ran into ourself
-    int idx = snake_cooridinate_to_index( head );
-    if( spaces[idx] )
-        snake_game_over( );
+    int idx = snake_cooridinate_to_index(head);
+    if(spaces[idx])     // 啥意思？？？
+        snake_game_over();
     spaces[idx] = true; // Mark the space as occupied
-    enqueue( head );
+    enqueue(head);      // 将头加入队列
     g_score += 10;
     
     // Check if we're eating the fruit
-    if( head.x == fruit.x && head.y == fruit.y )
+    if(head.x == fruit.x && head.y == fruit.y)
     {
         snake_draw_fruit( );
         g_score += 1000;
